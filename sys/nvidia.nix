2d4b16e292a9e04834af1850421c1c879953afc0
creator54 <hi.creator54@gmail.com>
2021-01-21 01:32:34 +0530

    enable powermanagement with nvidia

--- sys/nvidia.nix
+++ sys/nvidia.nix
@@ -25,6 +25,7 @@ in
       intelBusId = "PCI:0:2:0";
       nvidiaBusId = "PCI:1:0:0";
     };
+    powerManagement.enable = true;
     modesetting.enable = true;
     nvidiaPersistenced = true;
   };
