d9da1cb89a35ba5e3f811c44d0c9353f88987c77
creator54 <hi.creator54@gmail.com>
2021-01-22 01:04:55 +0530

    update

--- desktop/gnome.nix
+++ desktop/gnome.nix
@@ -10,5 +10,24 @@
       desktopManager.gnome3.enable = true;
     };
   };
+
+    environment.systemPackages = with pkgs;
+    with gnomeExtensions; [
+      gnome3.dconf-editor
+      gnome3.gnome-tweaks
+    ];
+
+  # exclude some default applications
+  environment.gnome3.excludePackages = with pkgs; [
+    gnome3.gnome-weather
+    gnome3.gnome-calendar
+    gnome3.gnome-maps
+    gnome3.gnome-contacts
+    gnome3.gnome-software
+    gnome3.totem
+    gnome3.epiphany
+  ];
+  programs.gnome-terminal.enable = false;
+  programs.geary.enable = false;
 }
 
