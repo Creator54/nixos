729be4e4ab91e43fddd514fadda80e4633659dc1
creator54 <hi.creator54@gmail.com>
2021-05-27 22:01:17 +0530

    Revert "switch to latest nightly build" bugs+missing some features ;( This reverts commit 205efafd30d35aa55320b0c79d821e11649cbff4.

--- sys/packages.nix
+++ sys/packages.nix
@@ -1,19 +1,9 @@
 { config, pkgs, ... }:
 
 {
-  nixpkgs.overlays =
-    let
-      # Change this to a rev sha to pin
-      moz-rev = "master";
-      moz-url = builtins.fetchTarball { url = "https://github.com/mozilla/nixpkgs-mozilla/archive/${moz-rev}.tar.gz";};
-      nightlyOverlay = (import "${moz-url}/firefox-overlay.nix");
-    in [
-      nightlyOverlay
-    ];
-
   environment.systemPackages = with pkgs; [
     wget vim htop feh nox git
-    latest.firefox-nightly-bin
+    firefox
     kitty
     fortune
     capitaine-cursors
