1b3dd06be68bc901a56166142fd2d4f7aaf97a61
creator54 <hi.creator54@gmail.com>
2021-06-23 21:32:49 +0530

    vm configs

---
+++ configuration.nix
@@ -0,0 +1,56 @@
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./desktop/awesome.nix
+    ];
+
+  boot = {
+    loader = {
+      systemd-boot.enable = true;
+      timeout = 0;
+      efi.canTouchEfiVariables = true;
+    };
+    #kernelParams = ["quiet" ];
+    #consoleLogLevel = 4;
+    #plymouth.enable = true;
+    cleanTmpDir = true;
+  };
+
+  # networking
+  networking = {
+    networkmanager.enable = true;
+    hostName = "CosPi";
+  };
+  
+  time.timeZone = "Asia/Kolkata";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_IN.UTF-8";
+  console = {
+    font = "Lat2-Terminus16";
+    keyMap = "us";
+    earlySetup = true;
+  };
+
+  # useraccount & properties
+  users = {
+    users.creator54 = {
+       isNormalUser = true;
+       extraGroups = [ "root" ];
+       shell = pkgs.fish;
+       initialHashedPassword = "jack@1234";
+    };
+  };
+
+  fileSystems."/".device = "/dev/sda4";
+
+  # Maintainence
+  nix.gc.automatic = true; 				# runs nix-collect-garbage which removes old unrefrenced packages
+  nix.gc.dates = "18:30";
+
+  system.stateVersion = "20.09";
+}
+
+
