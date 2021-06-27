3de9dbaec50080cf48de3b8bc28f2e805fcbb6ae
creator54 <hi.creator54@gmail.com>
2021-06-27 16:27:30 +0530

    add pantheon desktop

--- desktop/pantheon.nix
+++ desktop/pantheon.nix
@@ -8,11 +8,17 @@
         # pantheon.extraWingpanelIndicators
         # pantheon.extraSwitchboardPlugs 
       };
-      #displayManager.lightdm = {
-      #  enable = true;
-      #  greeters.pantheon.enable = false;
-      #};
+      displayManager = {
+        autoLogin.user = "creator54";
+        #lightdm = {  #gets enabled by default
+        #  enable = false;
+        #  greeters.pantheon.enable = false;
+        #};
+      };
     };
     pantheon.apps.enable = false; 
   };
+  # some apps still comes with the default setup so disabled here
+  environment.pantheon.excludePackages = with pkgs; [ gnome3.geary epiphany ];
+  environment.systemPackages = with pkgs; [ pantheon.elementary-code ];
 }
